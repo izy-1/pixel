6af627677b5b4c5d9de9609af4851acd09038a41
izy-1 <clauscdjen@gmail.com>
2023-10-25 20:15:25 +0200

    finish database sync

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,22 @@
-import {getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server'
-import {redirect} from 'next/navigation'
+import { db } from '@/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
 
-const Page = () => {
-    const {getUser} = getKindeServerSession()
-    const user = getUser()
+const Page = async () => {
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
 
-    if(!user || !user.id) redirect('/auth-callback?origin=dashboard')
+  if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
 
-    
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  })
 
-    return <div>{user.email}</div>
+  if(!dbUser) redirect('/auth-callback?origin=dashboard')
+
+  return 
 }
 
 export default Page
