33a630910d287ae415ec9b591868c41699dcc7a4
izy-1 <clauscdjen@gmail.com>
2023-10-27 14:22:08 +0200

    file upload done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import { cn } from '@/lib/utils'
 import Navbar from '@/components/Navbar'
 import Providers from '@/components/Providers'
 
-import "react-loading-skeleton/dist/skeleton.css"
+import 'react-loading-skeleton/dist/skeleton.css'
+import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,6 +28,7 @@ export default function RootLayout({
             'min-h-screen font-sans antialiased grainy',
             inter.className
           )}>
+          <Toaster />
           <Navbar />
           {children}
         </body>
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -39,24 +39,42 @@ export const appRouter = router({
       },
     })
   }),
+
+  getFile: privateProcedure
+    .input(z.object({ key: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx
+
+      const file = await db.file.findFirst({
+        where: {
+          key: input.key,
+          userId,
+        },
+      })
+
+      if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
+
+      return file
+    }),
+
   deleteFile: privateProcedure
     .input(z.object({ id: z.string() }))
-    .mutation(async ({ctx, input}) => {
-      const {userId} = ctx
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx
 
       const file = await db.file.findFirst({
         where: {
           id: input.id,
           userId,
-        }
+        },
       })
 
-      if(!file) throw new TRPCError({code: "NOT_FOUND"})
+      if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
 
       await db.file.delete({
         where: {
-         id: input.id,
-        }
+          id: input.id,
+        },
       })
 
       return file
