1be6e9e8444192ab7b8257c9abed488d6350bc43
izy-1 <clauscdjen@gmail.com>
2023-10-27 13:29:59 +0200

    setting up fileupload

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -7,10 +7,13 @@ import { Button } from './ui/button'
 import Dropzone from 'react-dropzone'
 import { Cloud, File } from 'lucide-react'
 import { Progress } from './ui/progress'
+import { useUploadThing } from '@/lib/uploadthing'
 
 const UploadDropzone = () => {
   const [isUploading, setIsUploading] = useState<boolean>(true)
   const [uploadProgress, setUploadProgress] = useState<number>(0)
+
+  const { startUpload } = useUploadThing('pdfUploader')
 
   const startSimulatedProgress = () => {
     setUploadProgress(0)
@@ -31,12 +34,17 @@ const UploadDropzone = () => {
   return (
     <Dropzone
       multiple={false}
-      onDrop={(acceptedFile) => {
+      onDrop={async (acceptedFile) => {
         setIsUploading(true)
 
         const progressInterval = startSimulatedProgress()
 
         // handle file uploading
+        const res = await startUpload(acceptedFile)
+
+        if(!res) {
+          
+        }
 
         clearInterval(progressInterval)
         setUploadProgress(100)
@@ -71,7 +79,10 @@ const UploadDropzone = () => {
 
               {isUploading ? (
                 <div className='w-full mt-4 max-w-xs mx-auto'>
-                  <Progress value={uploadProgress} className='h-1 w-full bg-zinc-200' />
+                  <Progress
+                    value={uploadProgress}
+                    className='h-1 w-full bg-zinc-200'
+                  />
                 </div>
               ) : null}
             </label>
