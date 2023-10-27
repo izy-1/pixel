c735c1847806f6306cf39c41c94ce8a43858ced2
izy-1 <clauscdjen@gmail.com>
2023-10-27 21:28:15 +0200

    load state

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -1,9 +1,11 @@
-"use client"
+'use client'
 
 import { trpc } from '@/app/_trpc/client'
 import ChatInput from './ChatInput'
 import Messages from './Messages'
-import { Loader2 } from 'lucide-react'
+import { ChevronLeft, Loader2, XCircle } from 'lucide-react'
+import Link from 'next/link'
+import { buttonVariants } from '../ui/button'
 
 interface ChatWrapperProps {
   fileId: string
@@ -37,15 +39,41 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
       </div>
     )
 
-    if(data?.status === "PROCESSING") return (
-        <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+  if (data?.status === 'PROCESSING')
+    return (
+      <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
         <div className='flex-1 flex justify-center items-center flex-col mb-28'>
           <div className='flex flex-col items-center gap-2'>
             <Loader2 className='h-8 w-8 text-blue-500 animate-spin' />
             <h3 className='font-semibold text-xl'>Processing PDF...</h3>
+            <p className='text-zinc-500 text-sm'>This won&apos;t take long.</p>
+          </div>
+        </div>
+
+        <ChatInput isDisabled />
+      </div>
+    )
+
+  if (data?.status === 'FAILED')
+    return (
+      <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+        <div className='flex-1 flex justify-center items-center flex-col mb-28'>
+          <div className='flex flex-col items-center gap-2'>
+            <XCircle className='h-8 w-8 text-red-500' />
+            <h3 className='font-semibold text-xl'>Too many pages in PDF</h3>
             <p className='text-zinc-500 text-sm'>
-              This won&apos;t take long.
+              Your <span className='font-medium'>Free</span> plan supports up to
+              5 pages per PDF.
             </p>
+            <Link
+              href='/dashboard'
+              className={buttonVariants({
+                variant: 'secondary',
+                className: 'mt-4',
+              })}>
+              <ChevronLeft className='h-3 w-3 mr-1.5' />
+              Back
+            </Link>
           </div>
         </div>
 
